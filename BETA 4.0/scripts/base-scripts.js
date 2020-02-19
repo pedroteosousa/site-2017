8eb0bcab09ef776b7f8cfe58dbc35bda631d9eac
Pedro Sousa <pedroteosousa@gmail.com>
2020-02-19 19:00:19 -0300

    change particles to squares

--- BETA 4.0/scripts/base-scripts.js
+++ BETA 4.0/scripts/base-scripts.js
@@ -1,75 +1,36 @@
 //Global variabes
 var canvas;
 var ctx;
-var particles = new Array();
-var particlesCount = 40;
-
+var squareCount
+var squares = []
 
 //Classes
-function Particle(x, y, xVelocity, yVelocity, radius, fieldRadius) {
-  this.x = x;
-  this.y = y;
-  this.xVelocity = xVelocity;
-  this.yVelocity = yVelocity;
-  this.radius = radius;
-  this.fieldRadius = fieldRadius;
+function Square(x, y, size, xVelocity, yVelocity, ang) {
+  this.x = x
+  this.y = y
+  this.size = size
+  this.xVelocity = xVelocity
+  this.yVelocity = yVelocity
 
-  var particleCache = document.createElement("canvas");
-  var particleCacheCtx = particleCache.getContext("2d");
+  this.cache = document.createElement("canvas")
+  this.cacheCtx = this.cache.getContext("2d")
 
-  particleCache.height = 50;
-  particleCache.width = 50;
+  this.cache.height = 3 * this.size
+  this.cache.width = 3 * this.size
 
-  particleCacheCtx.shadowBlur = 10;
-  particleCacheCtx.shadowColor = "rgba(64, 196, 255, 1)";
-
-  particleCacheCtx.fillStyle = "rgba(64, 196, 255, .1)";
-  
-  particleCacheCtx.beginPath();
-  particleCacheCtx.arc(particleCache.width / 2, 
-    particleCache.height / 2, 
-    radius / 2, 0, 2 * Math.PI
-  );
-  particleCacheCtx.fill();
-
-  particleCacheCtx.fillStyle = "rgba(64, 196, 255, 1)";
-  
-  particleCacheCtx.beginPath();
-  particleCacheCtx.arc(particleCache.width / 2, 
-    particleCache.height / 2, 
-    radius / 3, 0, 2 * Math.PI
-  );
-  particleCacheCtx.fill();
-
-  particleCacheCtx.shadowBlur = 5;
-
-  particleCacheCtx.fillStyle = "rgba(255, 255, 255, .8)";
-
-  particleCacheCtx.beginPath();
-  particleCacheCtx.arc(particleCache.width / 2, 
-    particleCache.height / 2, 
-    radius / 4, 0, 2 * Math.PI
-  );
-  particleCacheCtx.fill();
-
-  this.cache = particleCache;
+  this.cacheCtx.fillStyle = "rgba(64, 196, 355, 0.5)"
+  this.cacheCtx.translate(this.size, 0)
+  this.cacheCtx.rotate(ang * Math.PI / 180);
+  this.cacheCtx.fillRect(0, 0, this.size, this.size)
 }
-
 
 //Events
 function animationFrameRequested() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  for (var i = 0; i < particlesCount; i++) {
-    setParticlePosition(i);
-
-    // drawParticle(i);
-
-    // drawParticleField(i);
-
-    drawParticleNextConnections(i);
-
-    drawParticle(i);    
+  for (const square of squares) {
+    setSquarePosition(square);
+    drawSquare(square)
   }
   
   requestAnimationFrame(animationFrameRequested);  
@@ -82,131 +43,63 @@ function windowLoaded(event) {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
 
-  particlesCount = Math.ceil(window.innerWidth * window.innerHeight / 13500);
-  console.log(
-    window.innerWidth + ", " + window.innerHeight + " | " + 
-    canvas.offsetWidth + ", " + canvas.offsetHeight + " | " +
-    window.devicePixelRatio
-  );
+  squareCount = Math.ceil(window.innerWidth * window.innerHeight / 13500);
 
-  for (var i = 0; i < particlesCount; i++) {
-    particles[i] = generateParticle();
+  for (var i = 0; i < squareCount; i++) {
+    squares.push(generateSquare())
   }
 
   requestAnimationFrame(animationFrameRequested);
 }
 
 function windowResized(event) {
-  var newParticlesCount = Math.ceil(window.innerWidth * window.innerHeight / 13500);
+  var newSquareCount = Math.ceil(window.innerWidth * window.innerHeight / 13500);
 
   canvas.width = canvas.offsetWidth;
   canvas.height = canvas.offsetHeight;
 
-  if (newParticlesCount < particlesCount)
-    particles.splice(0, particlesCount - newParticlesCount);
+  if (newSquareCount < squareCount)
+    squares.splice(0, squareCount - newSquareCount);
 
-  while (newParticlesCount > particlesCount++)
-    particles.push(generateParticle());
+  while (newSquareCount > squareCount++)
+    squares.push(generateSquare());
     
-  particlesCount = newParticlesCount;
-  console.log(
-    window.innerWidth + ", " + window.innerHeight + " | " + 
-    canvas.offsetWidth + ", " + canvas.offsetHeight + " | " +
-    window.devicePixelRatio
-  );
+  squareCount = newSquareCount;
 }
-
 
 //Methods
-function drawParticle(i) {
+function drawSquare(square) {
   ctx.drawImage(
-    particles[i].cache,
-    particles[i].x - particles[i].cache.width / 2, 
-    particles[i].y - particles[i].cache.height / 2
-  );
+    square.cache,
+    square.x - square.cache.width / 2,
+    square.y - square.cache.height / 2
+  )
 }
 
-function drawParticleField(i) {
-  ctx.shadowBlur = 5;
-
-  ctx.strokeStyle = "rgba(255, 255, 255, .5)";
-
-  ctx.lineWidth = particles[i].radius / 8;
-
-  ctx.beginPath();
-  ctx.arc(particles[i].x + particles[i].radius / 2, 
-    particles[i].y + particles[i].radius / 2, 
-    particles[i].fieldRadius, 0, 2 * Math.PI
-  );
-  ctx.stroke();
-}
-
-function drawParticleNextConnections(i) {   
-  // ctx.shadowBlur = 5;
-  ctx.shadowColor = "rgba(64, 196, 355, 1)";   
-
-  for (var j = i + 1, n = 0; j < particlesCount && n < 4; j++) {
-    var distanceIJ = distance(particles[i].x, particles[i].y, particles[j].x, particles[j].y);
-
-    if (distanceIJ <= particles[i].fieldRadius || distanceIJ <= particles[j].fieldRadius) {
-      var opacity = (1 - (distanceIJ + Math.min(particles[i].fieldRadius, particles[j].fieldRadius)) / (particles[i].fieldRadius + particles[j].fieldRadius));
-      opacity = (1 / (0.75 + Math.exp(-2 * opacity)) - 0.25);
-      
-      ctx.strokeStyle = "rgba(255, 255, 255, " + opacity + ")";
-
-      ctx.lineWidth = 2 * opacity * (particles[i].radius / 8) * (particles[j].radius / 8);
-
-      ctx.beginPath();
-      ctx.moveTo(particles[i].x, particles[i].y);
-      ctx.lineTo(particles[j].x, particles[j].y);
-      ctx.stroke();
-      
-      ctx.strokeStyle = "rgba(64, 196, 355, " + (opacity / 2) + ")";
-
-      ctx.lineWidth = 4 * opacity * (particles[i].radius / 8) * (particles[j].radius / 8);
-
-      ctx.beginPath();
-      ctx.moveTo(particles[i].x, particles[i].y);
-      ctx.lineTo(particles[j].x, particles[j].y);
-      ctx.stroke();
-
-      n++;
-    }
-  }
-
-  ctx.shadowBlur = 0;
-} 
-
-function setParticlePosition(i) {  
+function setSquarePosition(square) {
   var offset = 200;
 
-  particles[i].x += particles[i].xVelocity;
-  particles[i].y += particles[i].yVelocity;
+  square.x += square.xVelocity
+  square.y += square.yVelocity
 
-  if (particles[i].x < 0 - offset) particles[i].x += canvas.offsetWidth + offset;
-  else if (particles[i].x > canvas.offsetWidth + offset) particles[i].x -= canvas.offsetWidth + offset;
-  if (particles[i].y < 0 - offset) particles[i].y += canvas.offsetHeight + offset;
-  else if (particles[i].y > canvas.offsetHeight + offset) particles[i].y -= canvas.offsetHeight + offset;
+  if (square.x < 0 - offset) square.x += canvas.offsetWidth + offset;
+  else if (square.x > canvas.offsetWidth + offset) square.x -= canvas.offsetWidth + offset;
+  if (square.y < 0 - offset) square.y += canvas.offsetHeight + offset;
+  else if (square.y > canvas.offsetHeight + offset) square.y -= canvas.offsetHeight + offset;
 }
 
-
-//Functions
-function distance(x1, y1, x2, y2) {      
-  return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
-}
-
-function generateParticle() {
-  return new Particle(
+function generateSquare() {
+  return new Square(
     randomInterval(0, canvas.width),
     randomInterval(0, canvas.height),
-    (randomInterval(.3, .6) - 1) * ((randomInterger(2) == 0) ? 1 : -1),
-    (randomInterval(.3, .6) - 1) * ((randomInterger(2) == 0) ? 1 : -1),
-    (randomInterval(0, 4) - 10) *  -1,
-    (randomInterval(0, 150) - 200) *  -1
-  );
+    randomInterval(5, 30),
+    randomInterval(.5, 1) * (randomInteger(2) == 0 ? -1 : 1),
+    randomInterval(.5, 1) * (randomInteger(2) == 0 ? -1 : 1),
+    randomInterval(0, 90)
+  )
 }
 
-function randomInterger(n) {
+function randomInteger(n) {
   return Math.floor(Math.random() *n);
 }
 
